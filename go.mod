45c1e56de9483b39791b4d5ae374a4feca65967b
7kaji <hirotonbuu@gmail.com>
2020-01-03 19:09:11 +0900

    hajimenoippo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/7kaji/hajimenoippo
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/7kaji/hajimenoippo/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+}
